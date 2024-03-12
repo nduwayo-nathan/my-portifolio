1cfbd0d72dd7178e5a87f06bdd4082db4c54e462
nduwayo-nathan <nduwayonathan5@gmail.com>
2024-03-12 16:18:04 +0200

    modifying

--- src/componets/portifolio.jsx
+++ src/componets/portifolio.jsx
@@ -1,62 +1,15 @@
-// import React from 'react';
-// import './portifolio.css';
 import project1Image from '../img/website.jpeg'; // Import your project images
 import project2Image from '../img/manager.jpeg';
 
-// const Portfolio = () => {
-//   const projects = [
-//     {
-//       title: 'Portifolio 1',
-//       Heading:"Heading",
-//       description: 'Description of Project 1',
-//       image: project1Image, // Replace with the path to your project image
-//       link: 'https://example.com/project1',
-      
-//     },
-//     {
-//       title: 'Portifolio 1',
-//       Heading:"Heading",
-//       description: 'Description of Project 1',
-//       image: project1Image, // Replace with the path to your project image
-//       link: 'https://example.com/project1',
-//     },
-//     // Add more projects as needed
-//   ];
 
-//   return (
-//     <section id="portfolioSection">
-//       <h2>Portfolio</h2>
-//       <div className="portifolio-container">
-//         {projects.map((project, index) => (
-//          <SwiperSlide>
-//          <div className="portifolioDiv" key={index}>
-//             <h3>{project.title}</h3>
-//             <img src={project.image} alt={`${project.title} Preview`} />
-//             <h4>{project.Heading}</h4>
-//             <p>{project.description}</p>
-//           </div>
-//           </SwiperSlide>
-//           
-//         ))}
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Portfolio;
-
-// import React, { useRef, useState } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 
 import './portifolio.css';
 
-// import required modules
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 const Portfolio = () => {
